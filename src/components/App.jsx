29bbf554468d321b0d94d00417108bd5ea0a80dd
Rammbest <igor-kutsenko@ukr.net>
2023-11-30 21:27:16 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import wait from '../images/wait.jpg';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        maxWidth: '1280px',
+        margin: '0 auto',
+        fontSize: 32,
+        color: '#58ec13d9',
       }}
     >
-      React homework template
+      Ukraine Auto rental service
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+
+          backgroundImage: `url(${wait})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      ></div>
     </div>
   );
 };
