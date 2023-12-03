40678812652f47741bb2685ecc2b1293449bdc78
ihor-kutsenko <96702664+ihor-kutsenko@users.noreply.github.com>
2023-12-03 16:46:15 +0000

    Deploying to gh-pages from @ ihor-kutsenko/Ukraine-AutoRental@2b73425abb8d051931ba46fb759917e5e85f0ff2 🚀

--- static/js/413.4b2af8e8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[413],{413:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("div",{children:"favorites"})})}}}]);
-//# sourceMappingURL=413.4b2af8e8.chunk.js.map
